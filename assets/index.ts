a45bfc79256a7db14b13bb577e1994e774388433
Saurav Gupta <sauravgupta@Sauravs-MacBook-Air.local>
2025-08-31 23:22:50 +0530

    changes for testing

--- assets/index.ts
+++ assets/index.ts
@@ -1,5 +1,3 @@
-// Centralized image exports
-// Tournament images
 export const TOURNAMENT_IMAGES = {
   FOOTBALL: require('./Tournament-1.png'),
   CRICKET: require('./Tournament-2.png'),
@@ -7,7 +5,7 @@ export const TOURNAMENT_IMAGES = {
   TABLE_TENNIS: require('./Tournamment-4.png'),
 };
 
-// App images
+// dbdsiu?
 export const APP_IMAGES = {
   ADAPTIVE_ICON: require('./adaptive-icon.png'),
   FAVICON: require('./favicon.png'),
@@ -15,10 +13,7 @@ export const APP_IMAGES = {
   SPLASH_ICON: require('./splash-icon.png'),
 };
 
-// We're using emoji icons directly in the components
-// No need for icon image files
 
-// Helper function to get tournament image by sport name
 export const getTournamentImageBySport = (sportName?: string) => {
   if (!sportName) return TOURNAMENT_IMAGES.FOOTBALL;
   
